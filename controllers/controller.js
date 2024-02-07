1f064ba88950c931821dbe20d339a7868086f8b6
Rizalfathi <rijalfahi303@gmail.com>
2024-02-07 13:11:16 +0700

    Lanjutan Express JS

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,19 +1,30 @@
-const siswa = require('../model/siswa');
+const SiswaModel = require('../model/siswa');
 
 const getAll = async (req, res) => {
     try {
-        const siswa = await siswa.find();
-        res.status(200).json(siswa);
+        const siswaList = await SiswaModel.find();
+        res.status(200).json(siswaList);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'server error'})
+        res.status(500).json({ message: 'server error' });
     }
 };
 
-const create = (req,res) => {
-    res.send(req.body)
-}
-
+const create = async (req, res) => {
+    const data = new SiswaModel({
+        nama: req.body.nama,
+        nisn: req.body.nisn,
+        alamat: req.body.alamat
+    });
+    try {
+        const savedData = await data.save();
+        console.log(savedData);
+        res.status(201).json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
 
 module.exports = {
     getAll,
